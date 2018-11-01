6d3407d94fd7011597432533ccdfc2ad7fd53bd4
faelborba <rafael.bds25@gmail.com>
2018-10-31 22:57:57 -0300

    criando as classes necessarias para funcionamento

--- src/main/java/br/edu/ulbra/election/candidate/model/Candidate.java
+++ src/main/java/br/edu/ulbra/election/candidate/model/Candidate.java
@@ -1,5 +1,8 @@
 package br.edu.ulbra.election.candidate.model;
 
+import br.edu.ulbra.election.candidate.output.v1.ElectionOutput;
+import br.edu.ulbra.election.candidate.output.v1.PartyOutput;
+
 import javax.persistence.*;
 
 @Entity
@@ -21,5 +24,43 @@ public class Candidate {
     @Column(nullable = false)
     private PartyOutput partyOutput;
 
-    
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Long getNumberElection() {
+        return numberElection;
+    }
+
+    public void setNumberElection(Long numberElection) {
+        this.numberElection = numberElection;
+    }
+
+    public ElectionOutput getElectionOutput() {
+        return electionOutput;
+    }
+
+    public void setElectionOutput(ElectionOutput electionOutput) {
+        this.electionOutput = electionOutput;
+    }
+
+    public PartyOutput getPartyOutput() {
+        return partyOutput;
+    }
+
+    public void setPartyOutput(PartyOutput partyOutput) {
+        this.partyOutput = partyOutput;
+    }
 }
